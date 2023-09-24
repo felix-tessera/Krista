7636f5598141910f1a5745de03a3b72ae9a37126
felix-tessera <nikitarassvetov@mail.ru>
2023-09-24 14:03:14 +0300

    added full album loading, fized picker

--- krista_mobile_mobule/lib/sreens/custom_media_picker_screen.dart
+++ krista_mobile_mobule/lib/sreens/custom_media_picker_screen.dart
@@ -9,22 +9,68 @@ class MediaGrid extends StatefulWidget {
 }
 
 class _MediaGridState extends State<MediaGrid> {
-  List<AssetEntity> _mediaList = [];
+  List<Widget> _mediaList = [];
+  int currentPage = 0;
+  int lastPage = 0;
+  dynamic allAlbums = [];
+
   @override
   void initState() {
     super.initState();
     _fetchNewMedia();
   }
 
+  _handleScrollEvent(ScrollNotification scroll) {
+    if (scroll.metrics.pixels / scroll.metrics.maxScrollExtent > 0.33) {
+      if (currentPage != lastPage) {
+        _fetchNewMedia();
+      }
+    }
+  }
+
   _fetchNewMedia() async {
+    lastPage = currentPage;
     final PermissionState result = await PhotoManager.requestPermissionExtend();
     if (result.isAuth) {
       List<AssetPathEntity> albums =
-          await PhotoManager.getAssetPathList(onlyAll: true);
+          await PhotoManager.getAssetPathList(type: RequestType.image);
+      allAlbums = albums;
+//      debugPrint(albums.toString());
       List<AssetEntity> media =
-          await albums[0].getAssetListPaged(page: 0, size: 20);
+          await albums[0].getAssetListPaged(page: currentPage, size: 60);
+
+      List<Widget> temp = [];
+      for (var asset in media) {
+        temp.add(
+          FutureBuilder(
+            future: asset.thumbnailDataWithSize(const ThumbnailSize(200, 200)),
+            builder: (BuildContext context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.done) {
+                return Stack(
+                  children: <Widget>[
+                    Positioned.fill(
+                        child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(16),
+                        child: Image.memory(
+                          snapshot.data!,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ))
+                  ],
+                );
+              }
+              return Container();
+            },
+          ),
+        );
+      }
+
       setState(() {
-        _mediaList = media;
+        _mediaList.addAll(temp);
+        currentPage++;
       });
     } else {
       // fail
@@ -34,23 +80,29 @@ class _MediaGridState extends State<MediaGrid> {
 
   @override
   Widget build(BuildContext context) {
-    return GridView.builder(
-        itemCount: _mediaList.length,
-        gridDelegate:
-            const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
-        itemBuilder: (BuildContext context, int index) {
-          return FutureBuilder(
-            future: _mediaList[index]
-                .thumbnailDataWithSize(const ThumbnailSize(200, 200)),
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.done) {
-                return Image.memory(
-                  snapshot.data!,
-                );
-              }
-              return Container();
-            },
-          );
-        });
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 16, 15, 22),
+        title: const Text(
+          'Mock',
+          style: TextStyle(color: Color.fromARGB(255, 224, 224, 224)),
+        ),
+        centerTitle: true,
+      ),
+      backgroundColor: Color.fromARGB(255, 16, 15, 22),
+      body: NotificationListener<ScrollNotification>(
+        onNotification: (ScrollNotification scroll) {
+          _handleScrollEvent(scroll);
+          return true;
+        },
+        child: GridView.builder(
+            itemCount: _mediaList.length,
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                childAspectRatio: (1 / 1.4), crossAxisCount: 2),
+            itemBuilder: (BuildContext context, int index) {
+              return _mediaList[index];
+            }),
+      ),
+    );
   }
 }
