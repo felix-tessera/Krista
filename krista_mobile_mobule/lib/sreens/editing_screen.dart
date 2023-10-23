125b0e5277d41a1881a3e1fabb9c64ac28830ff6
felix-tessera <nikitarassvetov@mail.ru>
2023-10-23 12:26:03 +0300

    added function section

--- krista_mobile_mobule/lib/sreens/editing_screen.dart
+++ krista_mobile_mobule/lib/sreens/editing_screen.dart
@@ -22,6 +22,10 @@ class _EditingScreenState extends State<EditingScreen> {
   Color color2 = const Color.fromARGB(0, 0, 0, 0);
   bool isImageSave = false;
 
+  Widget editingSection1 = Container(
+    key: ValueKey(0),
+  );
+
   @override
   void initState() {
     super.initState();
@@ -187,22 +191,140 @@ class _EditingScreenState extends State<EditingScreen> {
             ],
           ),
         ),
-        body: PhotoView(
-          backgroundDecoration: BoxDecoration(
-              gradient: LinearGradient(
-                  colors: [color1, color2],
-                  begin: Alignment.bottomCenter,
-                  end: Alignment.topCenter)),
-          maxScale: PhotoViewComputedScale.covered * 2,
-          minScale: PhotoViewComputedScale.contained,
-          loadingBuilder: (context, event) {
-            return Container(
-              color: const Color.fromARGB(255, 16, 15, 22),
-              child: Center(
-                  child: Lottie.asset('assets/animations/logo_animation.json')),
-            );
-          },
-          imageProvider: FileImage(widget.selectedImage!),
-        ));
+        body: Stack(children: [
+          PhotoView(
+            backgroundDecoration: BoxDecoration(
+                gradient: LinearGradient(
+                    colors: [color1, color2],
+                    begin: Alignment.bottomCenter,
+                    end: Alignment.topCenter)),
+            maxScale: PhotoViewComputedScale.covered * 2,
+            minScale: PhotoViewComputedScale.contained,
+            loadingBuilder: (context, event) {
+              return Container(
+                color: const Color.fromARGB(255, 16, 15, 22),
+                child: Center(
+                    child:
+                        Lottie.asset('assets/animations/logo_animation.json')),
+              );
+            },
+            imageProvider: FileImage(widget.selectedImage!),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: SizedBox(
+                  height: 120,
+                  child: ListView(
+                    shrinkWrap: true,
+                    scrollDirection: Axis.horizontal,
+                    children: [
+                      GestureDetector(
+                        child: EditingSection(),
+                        onTap: () {
+                          setState(() {
+                            editingSection1 =
+                                EditingSection.thisSectionFunctions;
+                          });
+                        },
+                      ),
+                      EditingSection(),
+                      EditingSection(),
+                    ],
+                  ),
+                ),
+              ),
+              AnimatedSwitcher(
+                duration: const Duration(milliseconds: 300),
+                transitionBuilder: (child, animation) => SlideTransition(
+                  position: Tween<Offset>(
+                    begin: Offset(0, 1), // Slide from right to left
+                    end: Offset.zero,
+                  ).animate(animation),
+                  child: child,
+                ),
+                child: editingSection1,
+              )
+            ],
+          )
+        ]));
+  }
+}
+
+class EditingSection extends StatelessWidget {
+  EditingSection({
+    super.key,
+  });
+
+  static Widget thisSectionFunctions = SizedBox(
+    height: 100,
+    child: ListView(scrollDirection: Axis.horizontal, children: const [
+      EditingFunctionWidget(),
+      EditingFunctionWidget(),
+      EditingFunctionWidget(),
+      EditingFunctionWidget(),
+    ]),
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.only(left: 15.0, right: 15.0, bottom: 20),
+        child: Container(
+            width: 100,
+            height: 100,
+            color: Colors.black,
+            child: const Center(
+              child: Column(
+                children: [
+                  Icon(
+                    Icons.style,
+                    color: Color.fromARGB(255, 224, 224, 224),
+                    size: 70,
+                  ),
+                  Text(
+                    'Name',
+                    style: TextStyle(color: Color.fromARGB(255, 224, 224, 224)),
+                  )
+                ],
+              ),
+            )),
+      ),
+    );
+  }
+}
+
+class EditingFunctionWidget extends StatelessWidget {
+  const EditingFunctionWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 10.0, right: 10.0, bottom: 10),
+      child: Container(
+          width: 100,
+          height: 100,
+          color: Colors.black,
+          child: const Center(
+            child: Column(
+              children: [
+                Icon(
+                  Icons.style,
+                  color: Color.fromARGB(255, 224, 224, 224),
+                  size: 70,
+                ),
+                Text(
+                  'Name',
+                  style: TextStyle(color: Color.fromARGB(255, 224, 224, 224)),
+                )
+              ],
+            ),
+          )),
+    );
   }
 }
